cca8217eaa1ec781713a826c76cec44600be5a04
pranavcpawar <pranavcpawar@outlook.com>
2023-09-01 23:45:50 +0530

    change message

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <h1>App</h1>
+        <h1>Start here</h1>
       </div>
     </main>
   )
